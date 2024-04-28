df10155672013517e1c213184d7d051424a50058
CatsRang <bless2k@gmail.com>
2024-04-28 13:03:53 +0900

    type & variabel example added

---
+++ 01.type_var/01_basic_type.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ---- Integers
+	// int, int8, int16, int32(rune), int64
+	// uint, uint8, uint16, uint32, uint64
+	{
+		var n64 int64
+		n64 = 200000
+
+		fmt.Printf("> n64 = (%T)%v\n", n64, n64)
+
+		n1 := -100
+		fmt.Printf("> n1 = (%T)%v\n", n1, n1)
+	}
+
+	// ---- Floating Point Nubmers
+	// float32, float64
+	{
+
+	}
+
+	// ---- Complex Numbers
+	// complex64, complex128
+	{
+
+	}
+
+	// ---- Boolean
+	// bool
+
+	// ---- String, Rune
+	// string
+	{
+		for i, r := range "Hello" {
+			fmt.Printf("[%v] %c (%v)\n", i, r, r)
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Go101 - Basic Go Example Project
 by bless2k@gmail.com, April 28 2024
+
+## References
+- [Effective Go](https://go.dev/doc/effective_go)
